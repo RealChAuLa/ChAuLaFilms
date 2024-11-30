827bdf1d54a283db3730b67bb2f616125db12988
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2024-11-30 18:49:38 +0530

    Applied Small Changes

--- php/updateWithoutIMG.php
+++ php/updateWithoutIMG.php
@@ -7,17 +7,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $movieGenre = $_POST['EmovieGenre'];
     $movieLink = $_POST['EmovieLink'];
     $movieAbout = $_POST['EmovieAbout'];
-
-
     $sql = "UPDATE movietbl SET name='$movieName', year='$movieYear', genre='$movieGenre', description='$movieAbout',  magnet='$movieLink' WHERE movie_id='$movieId'";
-
    // $sql = "UPDATE movietbl SET (name, year, genre, description, magnet) VALUES ('$movieName', '$movieYear', '$movieGenre', '$movieAbout', '$movieLink') WHERE movie_id='$movieId'";
-
     if ($conn->query($sql)) {
         echo '<html><script>alert("Movie Edited Successfully"); setTimeout(function() { window.location.href = "../Admin.php"; }, 50);</script></html>';
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
     mysqli_close($conn);
 }
